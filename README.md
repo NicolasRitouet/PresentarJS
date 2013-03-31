7f2c8d5d23640150418780e163e3d45e20a0b307
NicolasRTT <nicolas@ritouet.com>
2013-04-01 01:54:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Presentar
+=========
+Yet another presentation tool, based on angularjs and [HTML5Now](http://code.google.com/p/html5wow/).  
+
---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('PresentarApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
